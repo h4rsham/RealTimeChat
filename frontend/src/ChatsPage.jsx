d908200cc31de842b903d57d8be2532427693336
h4rsham <harsham.latif@outlook.com>
2023-08-07 16:15:42 +0100

    Created chats page using react chat window pretty library and used props to configure and customise the behaviour

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,22 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+//     const chatProps = useMultiChatLogic(
+//         '617d57aa-0f7c-4046-96e3-cacddfdc45a8', 
+//         props.user.username, 
+//         props.user.secret
+//     );
+
+    return (
+    <div style={{ height: '100vh' }}>
+        <PrettyChatWindow
+            projectId='617d57aa-0f7c-4046-96e3-cacddfdc45a8'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{height: '100%'}}
+        />
+    </div>
+    )
 }
 
 export default ChatsPage;
