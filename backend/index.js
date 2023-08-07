34562e4d48a42cca3cb1c9caa5ed7f890b6e0c2e
h4rsham <harsham.latif@outlook.com>
2023-08-07 15:29:51 +0100

    Added basic code / request to create users, added dotenv to store private key

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express"); // runs http server
+const cors = require("cors"); // call server from any other origin
+const { default: axios } = require("axios");
+require("dotenv").config(); // read .env file
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+// check if user exists
+// if not, create user
+// generate secret
+// store secret
+// send secret to user
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": process.env.PRIVATE_KEY}}
+    )
+    return res.status(r.status).json(r.data)
+    } catch (e) {
+       return res.status(e.response.status).json(e.response.data)
+}
+});
+
+
+
+app.listen(3001); // start server on port 3001
